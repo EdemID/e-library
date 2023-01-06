f553f898ac490e0fdb64895e9036d0acd0f496cb
EdemID <00350035aA>
2023-01-06 17:32:28 +0500

    Create functionality for people

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.Size;
 
 
 public class Person {
+
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
@@ -16,19 +17,14 @@ public class Person {
     @Min(value = 0, message = "Age should be greater than 0")
     private int age;
 
-    @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Email should be valid")
-    private String email;
-
     public Person() {
 
     }
 
-    public Person(int id, String name, int age, String email) {
+    public Person(int id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
-        this.email = email;
     }
 
     public int getId() {
@@ -55,11 +51,4 @@ public class Person {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<br/>
+<hr/>
+<form action="/people" target="_blank">
+    <button>Go!</button>
+</form>
+</body>
+</html>
